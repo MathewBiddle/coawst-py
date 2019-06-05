e4d3af822041c1ddb878ff80908818df47e9abe6
Matt Biddle <mbiddle@whoi.edu>
2019-06-04 23:37:00 -0400

    adding more plotting tools

--- coawst_timeseries_plot.py
+++ coawst_timeseries_plot.py
@@ -14,12 +14,36 @@ import numpy as np
 import datetime
 import time
 import netCDF4
-import sys
+import pandas as pd
+#import sys
 #import os
 
-# point location
-x = 56 # 57
-y = 27
+## point location geo
+#lat_pt = 39.516345
+#lon_pt = -76.034109
+
+# Cindy's locations
+locs = pd.DataFrame(columns=['Site', 'lat', 'lon'])
+locs['Site'] = ['1','2','3','4','5',
+                'Lee7','Lee6','Lee5','Lee2.5','Lee2','Lee0','LeeS2',
+                'CBIBS','Tripod']
+locs['lat'] = [39.527,39.533,39.515,39.505,39.497,
+               39.414,39.380,39.346,39.197,39.135,39.061,38.757,
+               39.5396,39.4931]
+locs['lon'] = [-76.061,-76.061,-76.051,-76.039,-76.036,
+               -76.079,-76.088,-76.197,-76.311,-76.328,-76.328,-76.473,
+               -76.0741,-76.0341]
+locs['comment'] = ['Russ and Palinkas 2018','','Middle of bed','','',
+                   '','','','','','','',
+                   'CBIBS Susquehanna Flats','Larry tripod site']
+
+# get coords for Site of choice
+lat_pt, lon_pt = locs.loc[locs['Site'] == 'CBIBS', ['lat', 'lon']].values[0]
+
+## Using point location idx
+#x = 56   # 57
+#y = 27      #
+
 #dir='/Users/mbiddle/Documents/Personal_Documents/Graduate_School/Thesis/COAWST/COAWST_RUNS/COAWST_OUTPUT/Full_20110702_20111101'
 #dir = '/Users/mbiddle/Documents/Personal_Documents/Graduate_School/Thesis/COAWST/COAWST_RUNS/COAWST_OUTPUT/Full_20110906_20110926'
 dir = '/Volumes/Documents/COAWST_34_UPPER_CHES_FULL'
@@ -27,8 +51,18 @@ dir = '/Volumes/Documents/COAWST_34_UPPER_CHES_FULL'
 #dir = '/Users/mbiddle/Documents/Personal_Documents/Graduate_School/Thesis/COAWST/COAWST_RUNS/COAWST_OUTPUT/Full_20110714201800_20111031231800'
 inputfile = dir+'/upper_ches_his.nc'
 
-f = netCDF4.Dataset(inputfile,'r')
+f = netCDF4.Dataset(inputfile, 'r')
 
+try:
+    lat_pt
+    lon_pt
+except NameError:
+    print("Using indexes x, y = (%i, %i)" % (x, y))
+else:
+    print("Using geo-coords lat, lon = (%f, %f)" % (lat_pt, lon_pt))
+    x = np.abs(f.variables['lon_rho'][:, 1]-lon_pt).argmin()
+    y = np.abs(f.variables['lat_rho'][1, :]-lat_pt).argmin()
+#sys.exit()
 ocean_time = f.variables['ocean_time'][:]
 #lat = f.variables['lat_rho'][:][:]
 #lon = f.variables['lon_rho'][:][:]
@@ -48,11 +82,10 @@ h[x,y] = 100
 plt.figure(1)
 plt.pcolor(f.variables['lon_rho'][:][:],f.variables['lat_rho'][:][:],h)
 
-
 # plot variables as time series
 myFmt = DateFormatter("%m/%d")
 
-var2plot = ['zeta','velocity','Hwave','tke','mud+sand','rho','wetdry_mask_rho']
+var2plot = ['zeta','velocity','Hwave','tke','mud+sand','bed_thickness']
 
 fig, (ax) = plt.subplots(nrows=len(var2plot), ncols=1, sharex=True, figsize=(12, 8))
 fig.subplots_adjust(hspace=0.05)
@@ -102,6 +135,14 @@ for i, ax in enumerate(fig.axes):
         #ylims = ax2v.get_ylim()
         ax.set_ylim(-0.7, 0.7)
         ax2v.xaxis.set_major_formatter(myFmt)
+        ax.grid(True)
+    elif var2plot[i] == 'bed_thickness':
+        ax.stackplot(datetime_list,f.variables[var2plot[i]][:, 2, x, y],
+                     f.variables[var2plot[i]][:, 1, x, y],
+                     f.variables[var2plot[i]][:, 0, x, y])
+        ax.set_ylabel('%s' % (f.variables[var2plot[i]].name))
+        ax.xaxis.set_major_locator(mdates.DayLocator(interval=dayint))
+        ax.xaxis.set_major_formatter(myFmt)
         ax.grid(True)
     else:
         ax.plot_date(datetime_list, f.variables[var2plot[i]][:, x, y], xdate=True, linestyle='-', linewidth=1,
