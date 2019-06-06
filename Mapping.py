5b21403f7ce3c08ccc36d249dec78e1e1717e4b4
Matt Biddle <mbiddle@whoi.edu>
2019-06-06 16:22:54 -0400

    Added a mapping script

---
+++ Mapping.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 30 20:25:09 2017
+
+@author: matt
+"""
+
+import os
+os.environ["PROJ_LIB"] = "/anaconda3/envs/coawst/share/proj/"
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import netCDF4
+
+# Cindy's locations
+locs = pd.DataFrame(columns=['Site', 'lat', 'lon'])
+locs['Site'] = ['1','2','3','4','5',
+                'Lee7','Lee6','Lee5','Lee2.5','Lee2','Lee0','LeeS2',
+                'CBIBS','Tripod']
+locs['lat'] = [39.527,39.533,39.515,39.505,39.497,
+               39.414,39.380,39.346,39.197,39.135,39.061,38.757,
+               39.5396,39.4931]
+locs['lon'] = [-76.061,-76.061,-76.051,-76.039,-76.036,
+               -76.079,-76.088,-76.197,-76.311,-76.328,-76.328,-76.473,
+               -76.0741,-76.0341]
+locs['comment'] = ['Russ and Palinkas 2018','','Middle of bed','','',
+                   '','','','','','','',
+                   'CBIBS Susquehanna Flats','Larry tripod site']
+
+# setup Lambert Conformal basemap.
+lat_max = locs.loc[locs['Site'] == '3','lat']+.08
+lat_min = locs.loc[locs['Site'] == '3','lat']-.16
+lon_min = locs.loc[locs['Site'] == '3','lon']-.1
+lon_max = locs.loc[locs['Site'] == '3','lon']+.1
+#m = Basemap(llcrnrlon=lon_min,llcrnrlat=lat_min,urcrnrlon=lon_max,urcrnrlat=lat_max,
+#             resolution='i', projection='merc', lat_ts=locs['lat'].mean())
+m = Basemap(llcrnrlon=lon_min, llcrnrlat=lat_min, urcrnrlon=lon_max, urcrnrlat=lat_max,
+    resolution='i', projection='merc', lat_ts=locs.loc[locs['Site'] == '3', 'lat'], epsg=3395)
+
+#lon=list(locs['lon'])
+#lat=list(locs['lat'])
+
+#x,y = m(lon,lat)
+# draw coastlines.
+m.arcgisimage(service="Canvas/World_Light_Gray_Base",xpixels = 3000)
+
+#,5,color='r',edgecolors='k',latlon=True,linewidths=0.3)
+
+# for label, xpt, ypt in zip(locs['Site'], locs['lon'], locs['lat']):
+#     if label != 'Lee5':
+#         plt.annotate(xpt, ypt, label, size='x-small')
+
+#dir='/Users/mbiddle/Documents/Personal_Documents/Graduate_School/Thesis/COAWST/COAWST_RUNS/COAWST_OUTPUT/Full_20110702_20111101'
+#dir = '/Users/mbiddle/Documents/Personal_Documents/Graduate_School/Thesis/COAWST/COAWST_RUNS/COAWST_OUTPUT/Full_20110906_20110926'
+#dir = '/Users/mbiddle/Documents/Personal_Documents/Graduate_School/Thesis/COAWST/COAWST_RUNS/COAWST_OUTPUT/Full_20110719T23_20111101'
+dir = '/Volumes/Documents/COAWST_34_UPPER_CHES_FULL'
+#inputfile = dir+'/upper_ches_his.nc'
+#dir = '/Users/mbiddle/Documents/Personal_Documents/Graduate_School/Thesis/COAWST/COAWST_RUNS/COAWST_OUTPUT/Full_20110714201800_20111031231800'
+inputfile = dir+'/upper_ches_his.nc'
+f = netCDF4.Dataset(inputfile, 'r')
+lon = f.variables['lon_rho'][:][:]
+lat = f.variables['lat_rho'][:][:]
+h = f.variables['h'][:][:]
+mask = f.variables['mask_rho'][:][:]
+mask = np.ma.masked_equal(mask,0)
+h.mask = mask.mask
+plant_height = f.variables['plant_height'][0, 0, :, :]
+plant_height = np.ma.masked_outside(plant_height,0.3,1)
+#plant_height = np.ma.masked_greater(plant_height, 1)
+#plant_height = np.ma.masked_less(plant_height, 0.3)
+m.pcolormesh(lon, lat, h, latlon=True, cmap='ocean')
+m.colorbar()
+m.pcolormesh(lon, lat, plant_height*10, latlon=True, cmap='Greens',vmin=0,vmax=10)
+
+lon=list(locs['lon'])
+lat=list(locs['lat'])
+
+x,y = m(lon,lat)
+
+plt.scatter(x,y,s=50,marker='.',color='r',edgecolors='k',linewidths=0.3)
+for label, xpt, ypt in zip(locs['Site'], x, y):
+     if label != 'Lee5':
+         plt.text(xpt+500, ypt, label, size='x-small', bbox=dict(facecolor='white', alpha=0.5))
--- coawst_timeseries_plot.py
+++ coawst_timeseries_plot.py
@@ -10,7 +10,6 @@ from matplotlib import cm
 #from matplotlib.ticker import LinearLocator, FormatStrFormatter
 #import warnings
 import numpy as np
-#import numpy.ma as ma
 import datetime
 import time
 import netCDF4
@@ -38,7 +37,8 @@ locs['comment'] = ['Russ and Palinkas 2018','','Middle of bed','','',
                    'CBIBS Susquehanna Flats','Larry tripod site']
 
 # get coords for Site of choice
-lat_pt, lon_pt = locs.loc[locs['Site'] == 'CBIBS', ['lat', 'lon']].values[0]
+site = 'Lee7'
+lat_pt, lon_pt = locs.loc[locs['Site'] == site, ['lat', 'lon']].values[0]
 
 ## Using point location idx
 #x = 56   # 57
@@ -46,12 +46,28 @@ lat_pt, lon_pt = locs.loc[locs['Site'] == 'CBIBS', ['lat', 'lon']].values[0]
 
 #dir='/Users/mbiddle/Documents/Personal_Documents/Graduate_School/Thesis/COAWST/COAWST_RUNS/COAWST_OUTPUT/Full_20110702_20111101'
 #dir = '/Users/mbiddle/Documents/Personal_Documents/Graduate_School/Thesis/COAWST/COAWST_RUNS/COAWST_OUTPUT/Full_20110906_20110926'
+#dir = '/Users/mbiddle/Documents/Personal_Documents/Graduate_School/Thesis/COAWST/COAWST_RUNS/COAWST_OUTPUT/Full_20110719T23_20111101'
 dir = '/Volumes/Documents/COAWST_34_UPPER_CHES_FULL'
 #inputfile = dir+'/upper_ches_his.nc'
 #dir = '/Users/mbiddle/Documents/Personal_Documents/Graduate_School/Thesis/COAWST/COAWST_RUNS/COAWST_OUTPUT/Full_20110714201800_20111031231800'
 inputfile = dir+'/upper_ches_his.nc'
-
 f = netCDF4.Dataset(inputfile, 'r')
+
+roms_in_dir = '/Volumes/Documents/ROMS_INPUT_FILES'
+river_frc = roms_in_dir+'/river_frc.nc'
+
+f_river = netCDF4.Dataset(river_frc, 'r')
+river_time = f_river.variables['river_time'][:]
+river_transport = f_river.variables['river_transport'][:, 0]
+
+## Do some date conversions ##
+epoch_date = '%s %s'%(f_river.variables['river_time'].units.split(' ')[-2], f_river.variables['river_time'].units.split(' ')[-1])
+dt_obj = datetime.datetime.strptime(epoch_date, '%Y-%m-%d %H:%M:%S')
+time_diff = time.mktime(dt_obj.timetuple())-time.mktime(datetime.datetime(1970, 1, 1, 0, 0, 0).timetuple())
+river_datetime_list=[]
+for sec in river_time:
+    ts = sec/(12*3600)
+    river_datetime_list.append(datetime.datetime.fromtimestamp(sec+time_diff))
 
 try:
     lat_pt
@@ -66,7 +82,6 @@ else:
 ocean_time = f.variables['ocean_time'][:]
 #lat = f.variables['lat_rho'][:][:]
 #lon = f.variables['lon_rho'][:][:]
-h = f.variables['h'][:][:]
 
 ## Do some date conversions ##
 epoch_date = '%s %s'%(f.variables['ocean_time'].units.split(' ')[-2], f.variables['ocean_time'].units.split(' ')[-1])
@@ -78,14 +93,18 @@ for sec in ocean_time:
     datetime_list.append(datetime.datetime.fromtimestamp(sec+time_diff))
 
 # Verify point location
-h[x,y] = 100
+plant_height = f.variables['plant_height'][0, 0, :, :]
+plant_height = np.ma.masked_greater(plant_height, 1)
+plant_height[x, y] = 1
 plt.figure(1)
-plt.pcolor(f.variables['lon_rho'][:][:],f.variables['lat_rho'][:][:],h)
+plt.pcolor(f.variables['lon_rho'][:][:], f.variables['lat_rho'][:][:], plant_height)
+plt.title('Site %s' % site)
+
 
 # plot variables as time series
 myFmt = DateFormatter("%m/%d")
 
-var2plot = ['zeta','velocity','Hwave','tke','mud+sand','bed_thickness']
+var2plot = ['depth','velocity','Hwave','tke','mud+sand','bed_thickness','river_transport']
 
 fig, (ax) = plt.subplots(nrows=len(var2plot), ncols=1, sharex=True, figsize=(12, 8))
 fig.subplots_adjust(hspace=0.05)
@@ -93,65 +112,91 @@ dayint = 10
 for i, ax in enumerate(fig.axes):
     if (var2plot[i] == 'rho') or (var2plot[i] == 'salt') or (var2plot[i] == 'tke') or (var2plot[i] == 'mud_01') or \
             (var2plot[i] == 'sand_01') or (var2plot[i] == 'temp'):
-        ax.plot_date(datetime_list, f.variables[var2plot[i]][:, 0, x, y], xdate=True, linestyle='-', linewidth=1,
-                     marker='.', markersize=1)
+        ax.plot_date(datetime_list, f.variables[var2plot[i]][:, 0, x, y], xdate=True, linestyle='-', linewidth=0.5,
+                     marker='', markersize=1)
         ax.set_ylabel('%s' % (f.variables[var2plot[i]].name))  # , f.variables[var2plot[i]].units))
         ax.xaxis.set_major_locator(mdates.DayLocator(interval=dayint))
         ax.xaxis.set_major_formatter(myFmt)
         ax.grid(True)
+        xlim = ax.get_xlim()
 
     elif var2plot[i] == 'mud+sand':
-        ax.plot_date(datetime_list, f.variables['mud_01'][:, 0, x, y], xdate=True, linestyle='-',linewidth=1,
+        ax.plot_date(datetime_list, f.variables['mud_01'][:, 0, x, y], xdate=True, linestyle='-',linewidth=0.5,
                      marker='',markersize=1,color='b')
-        ax.set_ylabel('%s' % (f.variables['mud_01'].name),color='b')  #
+        ax.set_ylabel('mud_SSC',color='b')  #
         ax.xaxis.set_major_locator(mdates.DayLocator(interval=dayint))
         ax.xaxis.set_major_formatter(myFmt)
 
         ylims = ax.get_ylim()
         ax2v = ax.twinx()
-        ax2v.plot_date(datetime_list, f.variables['sand_01'][:, 0, x, y], xdate=True, linestyle='-', linewidth=1,
+        ax2v.plot_date(datetime_list, f.variables['sand_01'][:, 0, x, y], xdate=True, linestyle='-', linewidth=0.5,
                      marker='',markersize=1,color='r')
-        ax2v.set_ylabel('%s' % ('sand_01'),color='r')
+        ax2v.set_ylabel('%s' % ('sand_SSC'),color='r')
         ax2v.set_ylim(ylims)
         ax2v.xaxis.set_major_locator(mdates.DayLocator(interval=dayint))
         ax2v.xaxis.set_major_formatter(myFmt)
         ax.grid(True)
+
     elif var2plot[i] == 'velocity':
         #ax.plot_date(datetime_list,
         #             np.sqrt(np.add(f.variables['ubar_eastward'][:, x, y]**2, f.variables['vbar_northward'][:, x, y]**2)),
         #             xdate=True, linestyle='-', linewidth=1, marker='', markersize=1)
         ax.plot_date(datetime_list,f.variables['ubar_eastward'][:, x, y],
-                     xdate=True, linestyle='-', linewidth=1, marker='', markersize=1, color='b')
+                     xdate=True, linestyle='-', linewidth=0.5, marker='', markersize=1, color='b')
         ax.set_ylabel('%s' % (f.variables['ubar_eastward'].name), color='b')  #
         ax.xaxis.set_major_locator(mdates.DayLocator(interval=1))
         ax.xaxis.set_major_formatter(myFmt)
 
         ax2v = ax.twinx()
         ax2v.plot_date(datetime_list,f.variables['vbar_northward'][:, x, y],
-                       xdate=True, linestyle='-', linewidth=1, marker='', markersize=1, color='r')
+                       xdate=True, linestyle='-', linewidth=0.5, marker='', markersize=1, color='r')
         ax2v.set_ylabel('%s' % (f.variables['vbar_northward'].name), color='r')  # , f.variables[var2plot[i]].units))
         ax2v.xaxis.set_major_locator(mdates.DayLocator(interval=dayint))
-        ax2v.set_ylim(-0.7, 0.7)
+        #ylim = max(np.abs([np.min(ax2v.get_ylim()), np.max(ax.get_ylim())]))
+        #ax2v.set_ylim(-1*ylim, ylim)
+        ax2v.set_ylim(-0.6, 0.6)
         #ylims = ax2v.get_ylim()
-        ax.set_ylim(-0.7, 0.7)
+        #ax.set_ylim(-1*ylim, ylim)
+        ax.set_ylim(-0.6, 0.6)
         ax2v.xaxis.set_major_formatter(myFmt)
         ax.grid(True)
+
     elif var2plot[i] == 'bed_thickness':
         ax.stackplot(datetime_list,f.variables[var2plot[i]][:, 2, x, y],
                      f.variables[var2plot[i]][:, 1, x, y],
                      f.variables[var2plot[i]][:, 0, x, y])
         ax.set_ylabel('%s' % (f.variables[var2plot[i]].name))
+        ax.set_ylim(0.8, 1.1)
         ax.xaxis.set_major_locator(mdates.DayLocator(interval=dayint))
         ax.xaxis.set_major_formatter(myFmt)
         ax.grid(True)
+
+    elif var2plot[i] == 'river_transport':
+        ax.plot_date(river_datetime_list, river_transport * f_river.variables['river_transport'].shape[1],
+                     xdate=True, linestyle='-', linewidth=0.5,
+                     marker='', markersize=1)
+        ax.set_ylabel('river_trans')
+        ax.xaxis.set_major_locator(mdates.DayLocator(interval=dayint))
+        ax.xaxis.set_major_formatter(myFmt)
+        ax.grid(True)
+        ax.set_xlim(xlim)
+    elif var2plot[i] == 'depth':
+        ax.plot_date(datetime_list, f.variables['zeta'][:, x, y]+f.variables['h'][x,y], xdate=True, linestyle='-', linewidth=0.5,
+                     marker='', markersize=1)
+        ax.set_ylabel('water_depth')
+        ax.set_ylim(0,6)
+        ax.xaxis.set_major_locator(mdates.DayLocator(interval=dayint))
+        ax.xaxis.set_major_formatter(myFmt)
+        ax.grid(True)
+
     else:
-        ax.plot_date(datetime_list, f.variables[var2plot[i]][:, x, y], xdate=True, linestyle='-', linewidth=1,
+        ax.plot_date(datetime_list, f.variables[var2plot[i]][:, x, y], xdate=True, linestyle='-', linewidth=0.5,
                      marker='', markersize=1)
         ax.set_ylabel('%s' % (f.variables[var2plot[i]].name))#, f.variables[var2plot[i]].units))
         ax.xaxis.set_major_locator(mdates.DayLocator(interval=dayint))
         ax.xaxis.set_major_formatter(myFmt)
         ax.grid(True)
-fig.suptitle('Point %fN %fE' % (f.variables['lat_rho'][x, y], f.variables['lon_rho'][x, y]))
+fig.suptitle('Site %s @ %fN %fE' % (site, f.variables['lat_rho'][x, y], f.variables['lon_rho'][x, y]))
 #fig.axes[0].title('test')
 
 def stick_plot(time, u, v, **kw):
