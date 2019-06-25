31ea4c6937fe02b9b16d494d79b74c24fde03492
Matt Biddle <mbiddle@whoi.edu>
2019-06-25 10:06:28 -0400

    Adding discharge validation script

---
+++ comparisons/discharge_validation.py
@@ -0,0 +1,80 @@
+import netCDF4
+import pandas as pd
+import matplotlib.pyplot as plt
+import coawstpy
+import matplotlib.dates as mdates
+from matplotlib.dates import DateFormatter
+import numpy as np
+
+
+dir = '/Users/mbiddle/Documents/Personal_Documents/Graduate_School/Thesis/COAWST/COAWST_RUNS/COAWST_OUTPUT/Full_20110719T23_20111101'
+
+
+## river data
+river_frc = dir+'/river_frc.nc'
+f_river = netCDF4.Dataset(river_frc, 'r')
+river_time = f_river.variables['river_time'][:]
+river_transport = f_river.variables['river_transport'][:, 3]
+river_datetime_list=[]
+for sec in river_time:
+    river_datetime_list.append(
+        netCDF4.num2date(sec, units=f_river.variables['river_time'].units, calendar='standard'))
+#river_transport = f_river.variables['river_transport'][:] # (river_time, river)
+
+
+
+inputfile = dir+'/upper_ches_his.nc'
+f = netCDF4.Dataset(inputfile, 'r')
+ocean_time = f.variables['ocean_time'][:]
+lat = f.variables['lat_rho'][:][:]
+lon = f.variables['lon_rho'][:][:]
+datetime_list=[]
+for sec in ocean_time:
+    datetime_list.append(netCDF4.num2date(sec, units=f.variables['ocean_time'].units, calendar=f.variables['ocean_time'].calendar))
+
+#xlim = (datetime_list.min(), datetime_list.max())
+## point selection
+x = np.arange(20,30)
+y = np.array([1]*len(x))
+#x = 26
+#y = 1
+plant_height = f.variables['plant_height'][0, 0, :, :]
+plant_height = np.ma.masked_greater(plant_height, 1)
+plant_height[x, y] = 1
+plt.figure(1)
+plt.pcolor(f.variables['lon_rho'][:][:], f.variables['lat_rho'][:][:], plant_height)
+
+
+# calculate water column volume for points x and y
+h = f.variables['h'][:]
+zeta = f.variables['zeta'][:]
+height = h[x, y]+zeta[:, x, y]
+pm = f.variables['pm'][:] # width - x
+pn = f.variables['pn'][:]
+#coord = pm[x, y]*pn[x, y]
+#vol = height/coord
+sa_pt = height/pn[x, y] # surface area
+
+#ubar = f.variables['ubar_eastward'][:]
+vbar = f.variables['vbar_northward'][:] # only care about whats coming in
+#ubar_pt = ubar[:, x, y]
+vbar_pt = vbar[:, x, y]
+
+#sim_q = sa*np.sqrt(vbar_pt**2+ubar_pt**2) # velocity times surface area = discharge
+sim_q_pt = -1*sa_pt*vbar_pt
+sim_q_mean = np.mean(sim_q_pt,axis=1)
+#input_q = river_transport[:,3]
+#plt.figure()
+fig, ax = plt.subplots()
+myFmt = DateFormatter("%m/%d")
+dayint=10
+ax.plot_date(river_datetime_list, river_transport, label='Input', xdate=True, linestyle='', linewidth=0.5,
+                marker='.', markersize=1)
+ax.plot_date(datetime_list, sim_q_mean, label='COAWST', xdate=True, linestyle='-', linewidth=0.5,
+                marker='o', markersize=1)
+ax.xaxis.set_major_locator(mdates.DayLocator(interval=dayint))
+ax.xaxis.set_major_formatter(myFmt)
+ax.set_ylabel('Discharge (m3/s)')
+ax.legend()
+
+#plt.xlim(xlim)
--- transect_sediment_transport.py
+++ transect_sediment_transport.py
@@ -35,29 +35,19 @@ for sec in ocean_time:
 
 # identify the points to extract data
 # Susquehanna River mouth
-trans_name = 'S.R. Mouth'
-x = np.array([29,30,31,32])
-y = np.array([13,12,11,10])
+trans_name_sr = 'T1'
+x_sr = np.array([29,30,31,32])
+y_sr = np.array([13,12,11,10])
 
 # Turkey Point to Sandy Point
 #trans_name = 'Turkey Pt to Sandy Pt'
 #x = np.array(list(range(42,67)))  #
 #y = np.array([58]*len(x))
 
-try:
-    lat_pt
-    lon_pt
-except NameError:
-    print("Using indexes x, y = (%s, %s)" % (x, y))
-else:
-    print("Using geo-coords lat, lon = (%f, %f)" % (lat_pt, lon_pt))
-    x = np.abs(f.variables['lon_rho'][:, 1]-lon_pt).argmin()
-    y = np.abs(f.variables['lat_rho'][1, :]-lat_pt).argmin()
-
 # Verify point location
 plant_height = f.variables['plant_height'][0, 0, :, :]
 plant_height = np.ma.masked_greater(plant_height, 1)
-plant_height[x, y] = 1
+plant_height[x_sr, y_sr] = 1
 # plt.figure(1)
 # plt.pcolor(f.variables['lon_rho'][:][:], f.variables['lat_rho'][:][:], plant_height)
 # plt.title('%s Transect' % trans_name)
@@ -65,20 +55,33 @@ plant_height[x, y] = 1
 # calculate the total concentration at each time step for the transect (x,y) and depth.
 # (h+zeta)/(pm*pn) * mud_01_depth_sum(time,x,y)
 # sum over depth
-mud = np.sum(f.variables['mud_01'][:, :, x, y], axis=1)
-sand = np.sum(f.variables['sand_01'][:, :, x, y], axis=1)
+mud_01 = f.variables['mud_01'][:]#, :, x, y]
+mud_01_trans_sr = mud_01[:, :, x_sr, y_sr]
+mud_sr = np.sum(mud_01_trans_sr, axis=1)
+
+sand_01 = f.variables['sand_01'][:]
+sand_01_trans_sr = sand_01[:, :, x_sr, y_sr]
+sand_sr = np.sum(sand_01_trans_sr, axis=1)
+
 # calculate water column volume for points x and y
-height = f.variables['h'][x, y]+f.variables['zeta'][:, x, y]
-coord = f.variables['pm'][x, y]*f.variables['pn'][x, y]
-vol = height/coord
+h = f.variables['h'][:]
+zeta = f.variables['zeta'][:]
+height_sr = h[x_sr, y_sr]+zeta[:, x_sr, y_sr]
 
-mud_trans_conc = vol*mud
-mud_tot_conc = np.sum(np.sum(mud_trans_conc, axis=1), axis=1)  # kg
+#sys.exit()
+pm = f.variables['pm'][:]
+pn = f.variables['pn'][:]
+coord_sr = pm[x_sr, y_sr]*pn[x_sr, y_sr]
+vol_sr = height_sr/coord_sr
+#sys.exit()
+mud_trans_conc_sr = vol_sr*mud_sr
+mud_tot_conc_sr = np.sum(mud_trans_conc_sr, axis=1)/1000  # metric ton
 
-sand_trans_conc = vol*sand
-sand_tot_conc = np.sum(np.sum(sand_trans_conc, axis=1), axis=1)  # kg
+sand_trans_conc_sr = vol_sr*sand_sr
+sand_tot_conc_sr = np.sum(sand_trans_conc_sr, axis=1)/1000  # metric ton
 
-total_sediment = (np.sum(sand_tot_conc)+np.sum(mud_tot_conc))/1000  # metric ton
+total_sediment_sr = np.float(np.sum(sand_tot_conc_sr)+np.sum(mud_tot_conc_sr))  # metric ton
+print("Transect at the mouth of S.R. total suspended sediment mass = %e tons" % total_sediment_sr)
 # totals to 4.62 x 10^6 metric tons entering the bay
 #
 # From Palinkas 2014:
@@ -91,62 +94,63 @@ total_sediment = (np.sum(sand_tot_conc)+np.sum(mud_tot_conc))/1000  # metric ton
 #sys.exit()
 #vel_mag = np.sqrt(ubar**2 + vbar**2)
 
+# initialize the figure
 myFmt = DateFormatter("%m/%d")
 dayint = 10
-
 fig, (ax) = plt.subplots(nrows=3, ncols=1, sharex=True, figsize=(12, 8))
-fig.subplots_adjust(hspace=0.5)
-ax[0].plot_date(datetime_list,mud_tot_conc, label='mud_01',
-              xdate=True, linestyle=':', linewidth=0.5,
+fig.subplots_adjust(hspace=0.25)
+
+# start plotting
+ax[0].plot_date(datetime_list,mud_tot_conc_sr, label='mud_01',
+              xdate=True, linestyle='-', linewidth=0.5,
               marker='', markersize=1, color='r')
-ax[0].plot_date(datetime_list,sand_tot_conc,label='sand_01',
+ax[0].plot_date(datetime_list,sand_tot_conc_sr,label='sand_01',
               xdate=True, linestyle='--', linewidth=0.5,
               marker='', markersize=1, color='r')
-ax[0].set_ylabel('%s sed [kg]'%trans_name)
+ax[0].set_ylabel('%s sed [t]'%trans_name_sr)
 ax[0].xaxis.set_major_locator(mdates.DayLocator(interval=dayint))
 ax[0].xaxis.set_major_formatter(myFmt)
-ax[0].grid(True)
+#ax[0].grid(True)
 ax[0].legend(loc='upper left')
-ax[2].plot_date(datetime_list,(mud_tot_conc+sand_tot_conc)/1000, label='S.R.',
-              xdate=True, linestyle='--', linewidth=0.5,
+ax[2].plot_date(datetime_list,(mud_tot_conc_sr+sand_tot_conc_sr), label=trans_name_sr,
+              xdate=True, linestyle='-', linewidth=0.5,
               marker='', markersize=1, color='r')
-#ylim = ax[0].get_ylim()
-#sys.exit()
-#ax[2].plot_date(datetime_list,vel_mag,label=trans_name,
-#                xdate=True, linestyle='-', linewidth=0.5,
-#                marker='', markersize=1, color='r')
-# q = coawstpy.stick_plot(datetime_list, ubar, vbar, ax=ax[0])
-# ax[0].set_ylabel(trans_name)
-# ax[0].xaxis.set_major_locator(mdates.DayLocator(interval=dayint))
-# ax[0].xaxis.set_major_formatter(myFmt)
-# ax[0].grid(True)
-# ax[0].quiverkey(q, X=0.1, Y=0.85, U=1, label='%s 1 m/s' % trans_name, labelpos='N', coordinates='axes')
+
 
 # Turkey Point to Sandy Point
-trans_name = 'Turkey2Sandy'
-x = np.array(list(range(42,67)))  #
-y = np.array([58]*len(x))
+trans_name_t2s = 'T2'
+x_t2s = np.array(list(range(42,67)))  #
+y_t2s = np.array([58]*len(x_t2s))
 
 # verify location
-plant_height[x, y] = 1
+plant_height[x_t2s, y_t2s] = 2
+
 
 # calculate the total concentration at each time step for the transect (x,y) and depth.
 # (h+zeta)/(pm*pn) * mud_01_depth_sum(time,x,y)
 # sum over depth
-mud = np.sum(f.variables['mud_01'][:, :, x, y], axis=1)
-sand = np.sum(f.variables['sand_01'][:, :, x, y], axis=1)
+
+mud_01_trans_t2s = mud_01[:, :, x_t2s, y_t2s]
+mud_t2s = np.sum(mud_01_trans_t2s, axis=1)
+
+sand_01_trans_t2s = sand_01[:, :, x_t2s, y_t2s]
+sand_t2s = np.sum(sand_01_trans_t2s, axis=1)
+
 # calculate water column volume for points x and y
-height = f.variables['h'][x, y]+f.variables['zeta'][:, x, y]
-coord = f.variables['pm'][x, y]*f.variables['pn'][x, y]
-vol = height/coord
+height_t2s = h[x_t2s, y_t2s]+zeta[:, x_t2s, y_t2s]
 
-mud_trans_conc = vol*mud
-mud_tot_conc = np.sum(np.sum(mud_trans_conc, axis=1), axis=1)  # kg
+#sys.exit()
+coord_t2s = pm[x_t2s, y_t2s]*pn[x_t2s, y_t2s]
+vol_t2s = height_t2s/coord_t2s
+#sys.exit()
+mud_trans_conc_t2s = vol_t2s*mud_t2s
+mud_tot_conc_t2s = np.sum(mud_trans_conc_t2s, axis=1)/1000  # metric ton
 
-sand_trans_conc = vol*sand
-sand_tot_conc = np.sum(np.sum(sand_trans_conc, axis=1), axis=1)  # kg
+sand_trans_conc_t2s = vol_t2s*sand_t2s
+sand_tot_conc_t2s = np.sum(sand_trans_conc_t2s, axis=1)/1000  # metric ton
 
-total_sediment = (np.sum(sand_tot_conc)+np.sum(mud_tot_conc))/1000  # metric ton
+total_sediment_t2s = (np.sum(sand_tot_conc_t2s)+np.sum(mud_tot_conc_t2s))  # metric ton
+print("Transect Turkey Point to Sandy Point total suspended sediment mass = %e tons" % total_sediment_t2s)
 # totals to 4.62 x 10^6 metric tons exiting the bay
 #
 # From Palinkas 2014:
@@ -164,25 +168,25 @@ dayint = 10
 
 #fig, (ax) = plt.subplots(nrows=3, ncols=1, sharex=True, figsize=(12, 8))
 #fig.subplots_adjust(hspace=0.05)
-ax[1].plot_date(datetime_list,mud_tot_conc, label='mud_01',
-              xdate=True, linestyle=':', linewidth=0.5,
+ax[1].plot_date(datetime_list,mud_tot_conc_t2s, label='mud_01',
+              xdate=True, linestyle='-', linewidth=0.5,
               marker='', markersize=1, color='b')
-ax[1].plot_date(datetime_list,sand_tot_conc,label='sand_01',
+ax[1].plot_date(datetime_list,sand_tot_conc_t2s,label='sand_01',
               xdate=True, linestyle='--', linewidth=0.5,
               marker='', markersize=1, color='b')
-ax[1].set_ylabel('%s sed [kg]'%trans_name)
+ax[1].set_ylabel('%s sed [t]'%trans_name_t2s)
 ax[1].xaxis.set_major_locator(mdates.DayLocator(interval=dayint))
 ax[1].xaxis.set_major_formatter(myFmt)
-ax[1].grid(True)
+#ax[1].grid(True)
 ax[1].legend(loc='upper left')
 #ylim = ax[1].get_ylim()
 
-ax[2].plot_date(datetime_list,(mud_tot_conc+sand_tot_conc)/1000, label='Turkey Pt',
-              xdate=True, linestyle='--', linewidth=0.5,
+ax[2].plot_date(datetime_list,(mud_tot_conc_t2s+sand_tot_conc_t2s), label=trans_name_t2s,
+              xdate=True, linestyle='-', linewidth=0.5,
               marker='', markersize=1, color='b')
 ax[2].legend(loc='upper left')
-ax[2].set_ylabel('Total sediment [t]')
-
+ax[2].set_ylabel('sand+mud [t]')
+plt.suptitle('Total Suspended Sediment Mass')
 plt.figure()
 plt.pcolor(f.variables['lon_rho'][:][:], f.variables['lat_rho'][:][:], plant_height)
 plt.title('Transect Locations')
