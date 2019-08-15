1602760f7f8f068ec0ee9efa87a442b01891e0d6
Matt Biddle <mbiddle@whoi.edu>
2019-08-14 23:26:38 -0400

    Possible problem with the mask??

--- flux_calculations/water_flux_from_avg_calculation_check.py
+++ flux_calculations/water_flux_from_avg_calculation_check.py
@@ -9,6 +9,7 @@ import coawstpy
 import scipy.integrate as integrate
 from scipy import stats
 import math
+import numpy.ma as ma
 
 '''
 Computes the water flux across transects by computing the flux sum in North and East direction, then translating to
@@ -26,21 +27,27 @@ inputfile = dir+'/upper_ches_avg.nc'
 print('Reading %s...' % inputfile.split("/")[-1])
 f = netCDF4.Dataset(inputfile, 'r')
 
+## TODO check the mask
+f.set_auto_maskandscale(False)
+
 # Get the data we want
 ocean_time = f.variables['ocean_time'][:]
-
 lat = f.variables['lat_rho'][:]
 lon = f.variables['lon_rho'][:]
-
-Huon = f.variables['Huon'][0:-1,:,:,:]  # lon_u east-west (from panoply should be S)
-Hvom = f.variables['Hvom'][0:-1,:,:,:]  # lon_v north-south (from panoply should be E)
+mask_u = f.variables['mask_u'][:]
+mask_v = f.variables['mask_v'][:]
+Huon = f.variables['Huon'][:]  # lon_u east-west (from panoply should be S)
+ma.masked_greater(Huon,100000000000)
+#ma.set_fill_value(Huon, 0)
+Hvom = f.variables['Hvom'][:]  # lon_v north-south (from panoply should be E)
+ma.masked_greater(Hvom,10000000000)
+#ma.masked_where(mask_v == 0, Hvom)
+#ma.set_fill_value(Hvom, 0)
 
 mask_rho = f.variables['mask_rho'][:]
 
 #s_rho = f.variables['s_rho'][:]  # depth levels
 
-tx1 =0
-tx2 = -1
 #tx1 = 1202
 #tx2 = 1466
 #srho_angle = 3
@@ -62,8 +69,8 @@ transect['Tb']['y'] = np.array([0]*len(transect['Tb']['x']))
 # Susquehanna River mouth #
 ###########################
 transect['T1'] = dict()
-transect['T1']['x'] = np.array([29,30,31,32])
-transect['T1']['y'] = np.array([13,12,11,10])
+transect['T1']['x'] = np.array([27,28,29,30,31,32,33,34])
+transect['T1']['y'] = np.array([15,14,13,12,11,10,9,8])
 #transect['T1']['x'] = np.array([28,29,30,31,32,33])
 #transect['T1']['y'] = np.array([14,13,12,11,10,9])
 
@@ -71,26 +78,36 @@ transect['T1']['y'] = np.array([13,12,11,10])
 # Turkey Point to Sandy Point #
 ###############################
 transect['T2'] = dict()
-transect['T2']['x'] = np.array(list(range(42,67)))
+transect['T2']['x'] = np.array(list(range(41,68)))
 transect['T2']['y'] = np.array([58]*len(transect['T2']['x']))
-#fig, (ax) = plt.subplots(nrows=3, ncols=1, figsize=(12, 12))
+
+
+## Iterate through each transect
 for t in transect: # each transect
     fig, (ax) = plt.subplots(nrows=3, ncols=1, figsize=(12, 12))
+
+    # get index of interest
     x = transect[t]['x']
     y = transect[t]['y']
     for j in range(len(x)):
         mask_rho[x[j], y[j]] = 5
+
+    # collect data at index of interest
     transect[t]['Hvom'] = Hvom[:, :, x, y]
     transect[t]['Huon'] = Huon[:, :, x, y]
+
+    # sum across transect and depth
     transect[t]['fs'] = np.sum(Huon[:, :, x, y], axis=(1,2))
     transect[t]['fe'] = np.sum(Hvom[:, :, x, y], axis=(1,2))
+
+    # make a plot of raw south and raw east on top subplot
     ax[0].plot(transect[t]['fs'],label='fs')
     ax[0].plot(transect[t]['fe'],label='fe')
     ax[0].set_ylabel('Raw Flux [m3/s]')
     ax[0].legend()
-    # TODO plotting
-    ax[1].plot(transect[t]['fs'], transect[t]['fe'], marker='.', linestyle='')
 
+    # make plot of raw south vs raw east
+    ax[1].plot(transect[t]['fs'], transect[t]['fe'], marker='.', linestyle='')
     ax[1].set_xlabel('South Flux (fs) [m3/s]', fontsize=8)
     ax[1].set_ylabel('East Flux (fe) [m3/s]', fontsize=8)
     xmin = np.min([transect[t]['fs'].min(), transect[t]['fe'].min()])
@@ -101,21 +118,26 @@ for t in transect: # each transect
     ax[1].set_aspect('equal', 'box')
     if t is 'Tb':
         ax[1].set_yticks([5000, 10000, 15000])
+
+    # compute linear regression
     slope, intercept, r_value, p_value, std_err = stats.linregress(transect[t]['fs'], transect[t]['fe'])
     xs = np.array([xmin, xmax])
     ax[1].plot(xs, slope*xs+intercept, linestyle='-', color='k')
-#    theta = math.degrees(math.atan(slope))
+
+    # compute angle linear regression is ccw from x axis
     theta = math.atan(slope)
-    ax[1].text(8000, 8000, 'theta = %.1f' % theta)
-    # TODO work through trigonometry here:
+    ax[1].text(8000, 8000, 'theta = %.2f' % theta)
+
+    # rotate to theta in radians
     transect[t]['fd'] = transect[t]['fs']*np.cos(theta) + transect[t]['fe']*np.sin(theta)
     transect[t]['fa'] = -1*transect[t]['fs']*np.sin(theta) + transect[t]['fe']*np.cos(theta)
-    ax[2].plot(transect[t]['fa'],label='fa = -fs*sin(theta) + fe*cos(theta)')
+    ax[2].plot(transect[t]['fa'], label='fa = -fs*sin(theta) + fe*cos(theta)')
     ax[2].plot(transect[t]['fd'], label='fd = fs*cos(theta) + fe*sin(theta)')
     ax[2].set_ylabel('Rotated Flux [m/s]')
     ax[2].legend()
     fig.suptitle(t)
 
+    print('%s max flux = %g m3/s' % (t, transect[t]['fd'].max()))
 #fig.tight_layout()
 
 
