873cd29390aa657411383614c40bb0b32c8cb91a
Matt Biddle <mbiddle@whoi.edu>
2019-10-14 10:07:44 -0400

    computing range of tide.

---
+++ tide_calculations/tidal_range.py
@@ -0,0 +1,18 @@
+from scipy.signal import find_peaks
+import pandas as pd
+from pytides.tide import Tide
+import numpy as np
+import netCDF4
+import coawstpy
+import matplotlib.pyplot as plt
+import scipy.integrate as integrate
+
+point_data = coawstpy.get_point_data('veg')
+
+
+min_peaks, _ = find_peaks(point_data['CBIBS']['depth'] * -1, height=-6)
+max_peaks, _ = find_peaks(point_data['CBIBS']['depth'], height=0)
+
+plt.plot(point_data['CBIBS']['depth'])
+plt.plot(point_data['CBIBS']['depth'][max_peaks], "x")
+plt.plot(point_data['CBIBS']['depth'][min_peaks], 'o')
